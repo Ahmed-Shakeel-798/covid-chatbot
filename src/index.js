963c1f47299c9c81e4d8a22f2fb8a2e38fe83e51
Ahmed Shakeel <72567856+Ahmed-Shakeel-798@users.noreply.github.com>
2021-03-13 18:32:38 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ async function send_message(body) {
 async function setup_network() {
 	let public_url = await ngrok.connect(process.env.PORT);
 	console.log(`Public Url:${public_url}`);
-	let webhook_url = 'https://7a0cb684eab0.ngrok.io/webhook';
+	let webhook_url = 'https://api.maytapi.com/webhook/live/send/6e153c20-83a5-11eb-8eab-0d0526157443/webhook';
 	    //`${public_url}/webhook`;
 	let url = `${INSTANCE_URL}/${PRODUCT_ID}/setWebhook`;
 	let response = await rp(url, {
